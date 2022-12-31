d88644c3777b6d35a5090480efdb5c3805a42057
Tran Xuan Dan <dan.tx.work@gmail.com>
2022-12-31 08:06:11 +0700

    - Add Project

---
+++ Assets/Features/Game/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Configuration;
+using Entitas;
+
+public class GameController
+{
+    readonly Systems _systems;
+    readonly Contexts _contexts;
+
+    public GameController(Contexts contexts, IGameConfiguration configuration)
+    {
+        contexts.configuration.SetGameConfiguration(configuration);
+
+        _contexts = contexts;
+        _systems = new GameSystems(contexts);
+    }
+
+    public void Initialize()
+    {
+        _systems.Initialize();
+    }
+
+    public void Execute()
+    {
+        _systems.Execute();
+        _systems.Cleanup();
+    }
+}
